5589e68fe4027afdbdaeff437391575894c04ecf
moharanasudipt <moharanasudiptkumar@gmail.com>
2023-11-25 20:08:00 +0530

    Modified

--- src/main/java/in/org/cutm/quizbackend/Entity/Users.java
+++ src/main/java/in/org/cutm/quizbackend/Entity/Users.java
@@ -9,7 +9,7 @@ public class  Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+
     private String username;
 
     @Column(nullable = false)
